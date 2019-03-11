354159416992494943d4e1970b34ee750f5703ef
Riccardo <riccardo_london@live.it>
2019-03-11 10:22:14 +0000

    Automatically backed up by Learn

--- spec/blackjack_spec.rb
+++ spec/blackjack_spec.rb
@@ -80,7 +80,6 @@ describe "#hit?" do
   it "calls on #prompt_user then #get_user_input" do
     expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
     expect(self).to receive(:get_user_input).and_return("s")
-        binding.pry
     hit?(7)
   end
 
